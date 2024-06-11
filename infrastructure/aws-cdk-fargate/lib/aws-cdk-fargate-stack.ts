f836076384af454339ec809e92b1724380560a18
Bruno Kalthoff <b.kalthoff@reply.de>
2024-06-11 14:57:52 +0200

    Test assume role

--- infrastructure/aws-cdk-fargate/lib/aws-cdk-fargate-stack.ts
+++ infrastructure/aws-cdk-fargate/lib/aws-cdk-fargate-stack.ts
@@ -1,51 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 
-export class AwsCdkFargateStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsCdkFargateStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'FargateNodeJsVpc', {
-      maxAzs: 2,
-      natGateways: 1,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: 'ingress',
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: 'application',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
-        },
-      ],
+    const vpc = new ec2.Vpc(this, 'RbpAppVpc', {
+      maxAzs: 3,
     });
 
-    const loadbalancer = new ApplicationLoadBalancer(this, 'lb', {
+    const loadbalancer = new elbv2.ApplicationLoadBalancer(this, 'RbpAppLb', {
       vpc,
       internetFacing: true,
-      vpcSubnets: vpc.selectSubnets({
-        subnetType: ec2.SubnetType.PUBLIC,
-      }),
     });
 
-    const cluster = new ecs.Cluster(this, 'Cluster', {
+    const cluster = new ecs.Cluster(this, 'RbpAppCluster', {
       vpc,
-      clusterName: 'fargate-node-cluster',
+      clusterName: 'rbp-app-cluster',
     });
 
-    const repo = new ecr.Repository(this, 'Repo', {
-      repositoryName: 'fargate-nodejs-app',
+    new ecr.Repository(this, 'RbpEcrRepo', {
+      repositoryName: 'rbp-app-repo',
     });
 
     const executionRole = new iam.Role(this, 'ExecutionRole', {
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
       managedPolicies: [
-        ManagedPolicy.fromAwsManagedPolicyName(
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
           'service-role/AmazonECSTaskExecutionRolePolicy',
         ),
       ],
@@ -53,23 +40,19 @@ export class AwsCdkFargateStack extends Stack {
 
     new ecs_patterns.ApplicationLoadBalancedFargateService(
       this,
-      'FargateNodeService',
+      'RbpAppService',
       {
         cluster,
         taskImageOptions: {
           image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
-          containerName: 'nodejs-app-container',
-          family: 'fargate-node-task-defn',
-          containerPort: 80,
+          containerName: 'rbp-app',
+          containerPort: 3000,
           executionRole,
         },
         cpu: 256,
         memoryLimitMiB: 512,
-        desiredCount: 2,
-        serviceName: 'fargate-node-service',
-        taskSubnets: vpc.selectSubnets({
-          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
-        }),
+        desiredCount: 1,
+        serviceName: 'rbp-app-service',
         loadBalancer: loadbalancer,
       },
     );
